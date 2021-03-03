be8373fcf471a1d3c27cdd625984a6c41398aa11
Ari Toren-Herrinton <atorenherrinton@Aris-MacBook-Pro.local>
2021-03-03 10:31:33 -0800

    fixing url error

--- client/src/components/recipe-url/recipe-url.component.jsx
+++ client/src/components/recipe-url/recipe-url.component.jsx
@@ -17,7 +17,7 @@ const RecipeUrl = () => {
   };
 
   const handleSubmit = () => {
-    fetch("https://recipeaz.heroku.com/api", {
+    fetch("https://recipeaz.herokuapp.com/api", {
       method: "POST",
       cache: "no-cache",
       headers: {
