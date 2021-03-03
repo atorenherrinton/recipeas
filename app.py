7dabb5e8cd650cea93cc604aebbe590feb3e1119
Ari Toren-Herrinton <atorenherrinton@Aris-MacBook-Pro.local>
2021-03-03 10:08:39 -0800

    trying stuff out

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, static_folder='client/build', static_url_path='')
 cors = CORS(app)
 
 
-@app.route('/api', methods=['POST'])
+@app.route('/api')
 @cross_origin()
 def get_result():
     result = request.json
